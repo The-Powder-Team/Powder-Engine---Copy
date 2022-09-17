e7aebcf2ec0418d827f6b4fce7340eafdbb9fc0f
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2022-09-16 21:52:16 -0400

    actually its in pre-alpha currently
    no its not like dave and bambi
    but i had to because its just 1% complete

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -40,7 +40,7 @@ class MainMenuState extends MusicBeatState
 
 	public static var nightly:String = "";
 
-	public static var kadeEngineVer:String = "BETA" + nightly;
+	public static var kadeEngineVer:String = "PRE-ALPHA";
 	public static var gameVer:String = "0.2.7.1";
 
 	var magenta:FlxSprite;
